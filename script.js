fa18bf7ca1a8ce58161fedcfbbc4e955f45a631e
takefumi24 <t_ohba24@yahoo.co.jp>
2020-05-16 01:29:17 +0900

    reduceメソッド

--- script.js
+++ script.js
@@ -1,29 +1,7 @@
-function Field(value) {
-  this.value = value;
-}
+const numbers = [10, 20, 30];
+const sum = 0;
 
-Field.prototype.validate = function () {
-  return this.value.length > 0;
-}
-
-Field.prototype.validate = function () {
-  return this.value.length > 0;
-}
-const username = new Field('my_username');
-const password = new Field('my_password');
-const birthday = new Field('2010/10/10');
-
-const fields = [
-  username,
-  password
-];
-
-const formIsValid = fields.every(function (field) {
-  return field.validate();
-});
-
-if (formIsValid) {
-  // サーバーにリクエストを投げる
-} else {
-  // エラー表示をする
-}
+const addNumber = numbers.reduce(function (sum,number) {
+  return sum + number;
+},10)
+console.log(addNumber);
