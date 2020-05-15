60eef5c9c9013e0442a8ee771a12eefa266b607b
takefumi24 <t_ohba24@yahoo.co.jp>
2020-05-15 19:29:17 +0900

    mapメソッド

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
-// 数字の配列を用意する
-var numbers = [1, 2, 3, 4, 5];
-// 合計を保持する変数を用意する
-var sum = 0
+const numbers = [1, 2, 3];
+const doubledNumbers = [];
+// for (var i = 0; i < numbers.length; i++){
+//   doubledNumbers.push(numbers[i] * 2);
+// };
 
-function adder(number) {
-  sum += number;
-}
-// 配列の一つ一つの数字を合計に足す
-numbers.forEach(adder);
-// 合計を表示する
-sum;
-console.log(sum);
+const doubled = numbers.map(function (number) {
+  return number*2
+})
+console.log(doubled);
+console.log(doubledNumbers);
