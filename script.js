b81687b2a994fe1531557c42560b86c3d89a8e57
takefumi24 <t_ohba24@yahoo.co.jp>
2020-05-16 00:34:18 +0900

    every,someメソッド

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
-const posts = [
-  { id: 1, title: '古い橋' },
-  { id: 2, title: '新しい投稿' },
+const computers = [
+  {name:"Apple", ram:24},
+  {name:"Compaq", ram:4},
+  {name:"Acer", ram:32}
 ];
 
-const comment = { postId: 2, content: 'イイね！' };
+// 16GB ramが必要なソフト
+// 全て動かせるフラグ
+// どれかでは動かせるフラグ
+computers.every(function (computer) {
+  return computer.ram >= 16
+});
 
-function postForComment(posts, comment) {
-  return posts.find(function (post) {
-    return post.id === comment.postId;
-  });
-}
-
-console.log(postForComment(posts, comment));
+computers.some(function (computer) {
+  return computer.ram >= 16
+});
