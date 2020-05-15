89e9c71f9b7879e161d83b2ff09f643d30765c06
takefumi24 <t_ohba24@yahoo.co.jp>
2020-05-15 19:04:41 +0900

    数字を使った配列の出し方（コメント付)

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
-// for (const i = 0;)ではエラーになる→constで変数定義をしているから
-const colors = ['red', 'blue', 'green'];
-for (var i = 0; i < colors.length; i++){
-  console.log(colors[i]);
-}
+// 数字の配列を用意する
+var numbers = [1, 2, 3, 4, 5];
+// 合計を保持する変数を用意する
+var sum = 0
 
-colors.forEach(function (color) {
-  console.log(color);
-});
+function adder(number) {
+  sum += number;
+}
+// 配列の一つ一つの数字を合計に足す
+numbers.forEach(adder);
+// 合計を表示する
+sum;
+console.log(sum);
