4207e81b4e2cf1984efea47e7648a41b8e43d504
takefumi24 <t_ohba24@yahoo.co.jp>
2020-05-16 20:40:56 +0900

    分割代入の応用（配列+オブジェクト)

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
-const companies = [
-  'Google',
-  'Facebook',
-  'Uber'
+const points = [
+  [4,5],
+  [10,3],
+  [40,0]
 ];
 
-const [name, ...rest] = companies;
-console.log(name);
-console.log(rest);
+
+const re = points.map(([x, y]) => {
+  return { x, y };
+});
+
+console.log(re);
+
+
+
+// 最終的にオブジェクトとして管理したい
+// [
+//   {x:4,y:5},
+//   {x:10,y:3},
+//   {x:40,y:0},
+// ]
