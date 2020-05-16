65e2213c90350ddd61178cab34530fa63aba343b
takefumi24 <t_ohba24@yahoo.co.jp>
2020-05-16 20:12:54 +0900

    配列に対して分割代入

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
-const savedFile = {
-  extension: 'jpg',
-  name: 'profile',
-  size: 14040
-};
+const companies = [
+  'Google',
+  'Facebook',
+  'Uber'
+];
 
-function fileSummary({name,extension,size},{username}) {
-  return `${username}:${name}.${extension}の容量は${size}です。`
-}
-
-console.log(fileSummary(savedFile,{username: 'ken' }));
+const [name, ...rest] = companies;
+console.log(name);
+console.log(rest);
