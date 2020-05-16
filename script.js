9b09800461e4b76fc44c117286f90f36b8c97789
takefumi24 <t_ohba24@yahoo.co.jp>
2020-05-16 13:55:52 +0900

    テンプレートリテラル

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 // アロー関数を使ってリファクタリングしてみる
-const numbers = [1, 2, 3];
-
-numbers.map(function (number) {
-  return 2 * number;
-});
-
-numbers.map(number => 2 * number);
+const team = {
+  members: ['太郎', '花子'],
+  teamName: 'スーパーチーム',
+  teamSummary: function () {
+    return this.members.map((member)=> {
+      return `${member}は${this.teamName}の所属です}`;
+    });
+  }
+};
