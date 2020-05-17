9c783d61b9bea7d9f0575fa73aca26bda3036d9e
takefumi24 <t_ohba24@yahoo.co.jp>
2020-05-17 19:15:08 +0900

    generatorとは

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
-const Member = function (firstName, lastName) {
-  this.firstName = firstName;
-  this.lastName = lastName;
-};
+function* shopping() {
+  // 歩道
 
-const mem = new Member('邦彦', '山田');
-mem.getName = function () {
-  return `${this.lastName} ${this.firstName}`;
+  // 歩道を歩いてお店にいいく
+
+  // お店に到着したのでお金を持ってお店に入る
+  const stuffFromStore = yield 'お金';
+
+  // 家に歩いて帰る
+  return stuffFromStore;
 }
-console.log(mem.getName());
+// お店関連の世界
+const gen = shopping();
+console.log(gen.next());//家から歩道に出る
+console.log(gen.next('日用品'));//お店で買物をして日用品を持って歩道に
