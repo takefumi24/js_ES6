d5636d5990fde5298604bcb967315e72b35c2908
takefumi24 <t_ohba24@yahoo.co.jp>
2020-05-17 20:52:38 +0900

    generatorとSymbol.iterator

--- script.js
+++ script.js
@@ -1,15 +1,43 @@
-function* shopping() {
-  // 歩道
+const testingTeam = {
+  lead:'典子',
+  tester: '隆',
+  [Symbol.iterator]: function* () {
+    yield this.lead;
+    yield this.tester;
+  }
 
-  // 歩道を歩いてお店にいいく
-
-  // お店に到着したのでお金を持ってお店に入る
-  const stuffFromStore = yield 'お金';
-
-  // 家に歩いて帰る
-  return stuffFromStore;
 }
-// お店関連の世界
-const gen = shopping();
-console.log(gen.next());//家から歩道に出る
-console.log(gen.next('日用品'));//お店で買物をして日用品を持って歩道に
+
+const engineeringTeam = {
+  testingTeam,
+  size: 3,
+  department: '開発部',
+  lead: '太郎',
+  manager: 'ゆり',
+  engineer: '次郎',
+  [Symbol.iterator]: function* () {
+    yield this.lead;
+    yield this.manager;
+    yield this.engineer;
+    yield* this.testingTeam;
+
+  }
+};
+
+// function* TeamIterator(team) {
+//   yield team.lead;
+//   yield team.manager;
+//   yield team.engineer;
+//   yield* team.testingTeam;
+// };
+
+// function* TestingTeamIterator(team) {
+//   yield team.lead;
+//   yield team.tester;
+// }
+
+const names = []
+for (let name of engineeringTeam) {
+  names.push(name);
+}
+console.log(names);
