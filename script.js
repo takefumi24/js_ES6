c73ea26c4924d4d9c06e95eed98174cb3b0b0ab6
takefumi24 <t_ohba24@yahoo.co.jp>
2020-05-19 00:43:54 +0900

    【JS API】 XMLHttpRequest()について

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 function callApi() {
-  fetch("https://jsonplaceholder.typicode.com/users")
-    .then(function (res) {
-      return res.json();
-    })
-    .then(function (users) {
-      console.log(users);
-    })
+  const xhr = new XMLHttpRequest();
+  xhr.open("GET", "https://jsonplaceholder.typicode.com/users");
+  xhr.responseType = "json";
+  xhr.send();
+  xhr.onload = function () {
+    console.log(xhr.response);
+  }
 }
 callApi();
