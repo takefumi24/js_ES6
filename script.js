0ec68b0c3cd12c31fce5fb622f7d82336cb04dca
takefumi24 <t_ohba24@yahoo.co.jp>
2020-05-19 00:39:14 +0900

    【JS API】 fetch thenについて

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-async function callApi() {
-  const res = await fetch("https://jsonplaceholder.typicode.com/users");
-  const users = await res.json();
-  console.log(users);
+function callApi() {
+  fetch("https://jsonplaceholder.typicode.com/users")
+    .then(function (res) {
+      return res.json();
+    })
+    .then(function (users) {
+      console.log(users);
+    })
 }
 callApi();
