d10b63a3c177a72173c703d2bb206ba5eddf43a8
takefumi24 <t_ohba24@yahoo.co.jp>
2020-05-20 20:40:21 +0900

    JS 前準備完了

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
-function callApi() {
-  const xhr = new XMLHttpRequest();
-  xhr.open("GET", "https://jsonplaceholder.typicode.com/users");
-  xhr.responseType = "json";
-  xhr.send();
-  xhr.onload = function () {
-    console.log(xhr.response);
-  }
+let youtuber = {
+  list: {
+    business: {
+      youtuber: [
+        { name: "dan", age: 17, teachPrograming() { } },
+        {name:"あっちゃん",age:37,techHistrory(){}}
+      ],
+      teach() {}
+    },
+
+    entertainment: {
+      youtuber: [{ name: "ヒカキン" }, { name: "はじめしゃちょー" }],
+      makeSmile(){}
+    }
+  },
+  plan() { },
+  uploadVideo(){}
 }
-callApi();
