9b2c9cbf6877e3280bdeda6db542226fc234b738
takefumi24 <t_ohba24@yahoo.co.jp>
2020-05-21 19:38:30 +0900

    JS　変数とは？

--- script.js
+++ script.js
@@ -1,32 +1,9 @@
-let youtuber = {
-  list: {
-    business: {
-      youtuber: [
-        { name: "dan", age: 17, teachPrograming() {} },
-        {name:"あっちゃん",age: 37,techHistrory(){}}
-      ],
-      teach() {}
-    },
+// Q.変数とは ?
+// A.データを保存しておくための箱
+// →可読性を上げるためにも大事
 
-    entertainment: {
-      youtuber: [{ name: "ヒカキン" }, { name: "はじめしゃちょー" }],
-      makeSmile(){}
-    }
-  },
-  plan() { },
-  uploadVideo(){}
-}
 
-// windowオブジェクトの中にあるconsoleプロパティでlogメソッド
-window.console.log();
-
-// window.は省略可能
-console.log();
-document.getElementById();
-
-//大きさを確認できる
-window.screen.width;
-window.screen.height;
-
-//ブラウザを更新
-window.location.reload
+const foo = document.getElementById("foo");
+const foodWidth = foo.offsetWidth;
+const foodHeight = foo.offsetHeight;
+const fooArea = foodWidth * foodHidth;
