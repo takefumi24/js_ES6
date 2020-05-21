f898de6e426119ffaa3749553507958f623033da
takefumi24 <t_ohba24@yahoo.co.jp>
2020-05-21 20:42:38 +0900

    関数式、匿名関数

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-function islessThan(text) {
+function isTweetable(text) {
   return text.length <= 10;
 }
+
+//関数式,匿名関数
+const isTweetable = function (text) {
+  return text.length <= 10;
+};
 
 function alertTweetable(text) {
   if (islessThan(text)) {
