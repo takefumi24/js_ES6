3d89cefe3530e9be2e5eb11f950baf4fa9af7e16
takefumi24 <t_ohba24@yahoo.co.jp>
2020-05-23 19:33:16 +0900

    条件演算子に書き換える

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 'use strict';
 
-console.log(parseInt('6',10) + 4);
+const score = 82;
+
+// if (score >= 80) {
+//   console.log('Great!');
+// } else {
+//   console.log('OK...');
+// }
+
+//上記のものを条件演算子に書き換える
+score >= 80 ? console.log('Great') : console.log('OK...');
