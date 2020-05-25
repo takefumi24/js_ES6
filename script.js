4c251ff41483b5978eec7de9d6872b7ccbbb0195
takefumi24 <t_ohba24@yahoo.co.jp>
2020-05-25 10:44:46 +0900

    スプレット構文で合計を求める問題

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
-'use strict';
+"use strict";
 
-//1から6までのランダムな整数値を作りなさい
-
+// スプレット構文を使って合計値を求める
 {
-  console.log(Math.floor(Math.random() * 6) + 1);
+  const otherScores = [10, 20];
+  const scores = [80, 90, 40, 70, ...otherScores];
+  console.log(scores);
+
+  function sum(a, b) {
+    console.log(a + b);
+  }
+
+  sum(...otherScores);
 }
