1cc4adc0b05fc888557b793167595b8a51e84d26
takefumi24 <t_ohba24@yahoo.co.jp>
2020-05-25 10:49:58 +0900

    分割代入とレスト構文

--- script.js
+++ script.js
@@ -1,14 +1,11 @@
 "use strict";
 
-// スプレット構文を使って合計値を求める
+// 分割代入とレスト構文
 {
-  const otherScores = [10, 20];
-  const scores = [80, 90, 40, 70, ...otherScores];
-  console.log(scores);
-
-  function sum(a, b) {
-    console.log(a + b);
-  }
-
-  sum(...otherScores);
+  const scores = [80, 90, 40, 70];
+// レスト構文
+  const [a, b, ...others] = scores;
+  console.log(a);
+  console.log(b);
+  console.log(others);
 }
