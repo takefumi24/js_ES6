63441bd1903d2501872e08072e0ade0f44b4d37c
takefumi24 <t_ohba24@yahoo.co.jp>
2020-05-26 12:30:05 +0900

    joinメソッド,splitメソッド

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 "use strict";
 
 {
-  //substringメソッド
-  const str = 'hello';
-  console.log(str.substring(2, 4));
-  //値の取得ができるだけ！！
-  console.log(str[1]);
+  const d = [2019, 11, 14];
+  //繋げる
+  console.log(d.join('/'));
+  //時間、分、秒を文字列の配列に
+  const t = "17:08:24";
+  console.log(t.split(":"));
+  //分割代入を使って配列に
+  const [hour, minute, second] = t.split(':');
+  console.log(hour);
+  console.log(minute);
+  console.log(second);
 }
