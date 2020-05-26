e36dac844f70bba871379f8d6b3273e50f62525b
takefumi24 <t_ohba24@yahoo.co.jp>
2020-05-26 12:14:06 +0900

    substringメソッド

--- script.js
+++ script.js
@@ -1,23 +1,9 @@
 "use strict";
 
 {
-  const point = {
-    x: 100,
-    y:180,
-  }
-
-  //Object.keys();
-  const keys = Object.keys(point);
-  keys.forEach(key => {
-    console.log(`Key:${key} Value: ${point[key]}`);
-  });
-
-  //配列とオブジェクトの組み合わせ
-  const points = [
-    { x:30,y:20},
-    { x:30,y:50},
-    { x:30,y:90},
-  ]
-
-  points[1].y
+  //substringメソッド
+  const str = 'hello';
+  console.log(str.substring(2, 4));
+  //値の取得ができるだけ！！
+  console.log(str[1]);
 }
