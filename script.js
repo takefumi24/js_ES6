85f5e566a0dd1b06d013f74828665d9a719b4f4d
takefumi24 <t_ohba24@yahoo.co.jp>
2020-05-27 11:54:22 +0900

    指定した時間ごとに処理を実行する

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 "use strict";
 
 {
-  // 年月日をコンソールに表示
-  const d = new Date(1962, 6); // 1962-07-07
-  // d.setDate(24); // 1962-07-24
-  // console.log(d);
-  d.setDate(33); // 1962-08-01
-  d.setDate(22); // 1962-08-22
-  d.setDate(0); // 1962-07-31
-  d.setDate(98); // 1962-10-06
-  d.setDate(-50); // 1962-08-11
-  console.log(d);
+  let i = 0;
+  function showTime() {
+    console.log(new Date());
+    i++;
+    if (i > 2) {
+      clearInterval(intervalId);
+    }
+  }
+  //1秒ごとに繰り返される
+  const intervalId = setInterval(showTime,1000);
 }
