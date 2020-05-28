06994f378d7d720d3bcbc9011d6149affa5d9d7e
takefumi24 <t_ohba24@yahoo.co.jp>
2020-05-28 19:27:25 +0900

    Web API完了

--- script.js
+++ script.js
@@ -1,42 +1,12 @@
 "use strict";
 
 {
-  class Post{
-    constructor(text) {
-      this.text = text;
-      this.likeCount = 0;
-    }
-
-    show() {
-      console.log(`${this.text}-${this.likeCount}イイね`);
-    }
-
-    like(){
-      this.likeCount++;
-      this.show();
-    }
-
-  }
-  class SponsoredPost extends Post{
-    constructor(text, sponsor) {
-      super(text);
-      this.sponsor = sponsor
-    }
-
-    show() {
-      super.show();
-      console.log(`..sponsored by ${this.sponsor}`);
-    }
-
-  }
-
-  const posts = [
-    new Post("おはようございます"),
-    new Post("今日も１日積み上げます！！"),
-    new SponsoredPost('1日を有意義に過ごそう！！', 'Filppy')
-  ];
-
-  posts[2].show();
-  posts[2].like();
+  const button = document.getElementById("addBtn");
+  const lists = document.getElementById("lists");
+  button.addEventListener('click', async() => {
+    const res = await fetch("https://jsonplaceholder.typicode.com/users");
+    const users = await res.json();
+    console.log(users);
+  });
 
 }
