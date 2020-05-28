0a55d53652471950fef3e67c0bb9494c95d17bbb
takefumi24 <t_ohba24@yahoo.co.jp>
2020-05-28 12:00:33 +0900

    クラスの拡張

--- script.js
+++ script.js
@@ -16,19 +16,33 @@
       this.show();
     }
 
-  //静的メソッド
-    static showInfo() {
-      console.log('thisは使えません！');
+  }
+  class SponsoredPost{
+    constructor(text, sponsor) {
+      this.text = text;
+      this.likeCount = 0;
+      this.sponsor = sponsor
     }
+
+    show() {
+      console.log(`${this.text}-${this.likeCount}イイね`);
+      console.log(`..sponsored by ${this.sponsor}`);
+    }
+
+    like(){
+      this.likeCount++;
+      this.show();
+    }
+
   }
 
   const posts = [
     new Post("おはようございます"),
     new Post("今日も１日積み上げます！！"),
+    new SponsoredPost('1日を有意義に過ごそう！！', 'Filppy')
   ];
 
-  // posts[0].like();
-  // posts[0].show();
-  // posts[1].show();
-  Post.showInfo();
+  posts[2].show();
+  posts[2].like();
+
 }
