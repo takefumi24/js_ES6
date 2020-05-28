976eb63328500ffc034a8a7d59b2e452481b88a5
takefumi24 <t_ohba24@yahoo.co.jp>
2020-05-28 11:25:30 +0900

    クラスからオブジェクト生成

--- script.js
+++ script.js
@@ -1,24 +1,22 @@
 "use strict";
 
 {
+  class Post{
+    constructor(text) {
+      this.text = text;
+      this.likecount = 0;
+    }
+
+    show() {
+      console.log(`${this.text}-${this.likecount}イイね`);
+    }
+  }
+
   const posts = [
-    {
-      text: "おはようございます",
-      likecount: 0,
-      show() {
-        console.log(`${this.text}-${this.likecount}イイね`);
-      },
-    },
-    {
-      text: "今日も１日積み上げます！！",
-      likecount: 0,
-      show() {
-        console.log(`${this.text}-${this.likecount}イイね`);
-      },
-    },
+    new Post("おはようございます"),
+    new Post("今日も１日積み上げます！！"),
   ];
 
-  // show(posts[0]);
   posts[0].show();
   posts[1].show();
 
