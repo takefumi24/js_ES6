fa31fc83721dd3f30482feefa7d5bc7a0f8213f2
takefumi24 <t_ohba24@yahoo.co.jp>
2020-05-28 19:40:33 +0900

    Dom操作(li要素を追加)

--- script.js
+++ script.js
@@ -3,10 +3,16 @@
 {
   const button = document.getElementById("addBtn");
   const lists = document.getElementById("lists");
-  button.addEventListener('click', async() => {
+  button.addEventListener('click', async () => {
+    //データのやりとり
     const res = await fetch("https://jsonplaceholder.typicode.com/users");
     const users = await res.json();
-    console.log(users);
+
+    //DOM操作
+
+    const list = document.createElement('li');
+    list.innerText = 'foo';
+    lists.appendChild(list);
   });
 
 }
