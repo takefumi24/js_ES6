6e9c717d171d145287aec824a2cef080abea0d5a
takefumi24 <t_ohba24@yahoo.co.jp>
2020-05-29 11:10:10 +0900

    【リファクタリング】順序の整理

--- script.js
+++ script.js
@@ -4,13 +4,6 @@
   const button = document.getElementById("addBtn");
   const lists = document.getElementById("lists");
 
-  async function getUsers() {
-    //データのやりとり
-    const res = await fetch("https://jsonplaceholder.typicode.com/users");
-    const users = await res.json();
-    return users;
-  }
-
   function addList(user) {
     // li要素を追加
     const list = document.createElement("li");
@@ -18,12 +11,20 @@
     lists.appendChild(list);
   }
 
+
+  async function getUsers() {
+    //データのやりとり
+    const res = await fetch("https://jsonplaceholder.typicode.com/users");
+    const users = await res.json();
+    return users;
+  }
+
+
   async function listUsers() {
     const users = await getUsers();
     users.forEach(addList);
   }
 
-  button.addEventListener("click", listUsers);
-
   window.addEventListener("load", listUsers);
+  button.addEventListener("click", listUsers);
 }
