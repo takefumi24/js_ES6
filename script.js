967cbb7a6ab341511dde290b4ecc3a7462b5f6c7
takefumi24 <t_ohba24@yahoo.co.jp>
2020-05-29 11:07:35 +0900

    【リファクタリング】　同じ記述を共通化する、処理を分解する

--- script.js
+++ script.js
@@ -3,32 +3,27 @@
 {
   const button = document.getElementById("addBtn");
   const lists = document.getElementById("lists");
-  button.addEventListener('click', async () => {
+
+  async function getUsers() {
     //データのやりとり
     const res = await fetch("https://jsonplaceholder.typicode.com/users");
     const users = await res.json();
+    return users;
+  }
 
-    //DOM操作
-    users.forEach((user) => {
-      // li要素を追加
-      const list = document.createElement('li');
-      list.innerText = user.name;
-      lists.appendChild(list);
-    });
-  });
+  function addList(user) {
+    // li要素を追加
+    const list = document.createElement("li");
+    list.innerText = user.name;
+    lists.appendChild(list);
+  }
 
-  window.addEventListener("load", async () => {
-    //データのやりとり
-    const res = await fetch("https://jsonplaceholder.typicode.com/users");
-    const users = await res.json();
+  async function listUsers() {
+    const users = await getUsers();
+    users.forEach(addList);
+  }
 
-    //DOM操作
-    users.forEach((user) => {
-      // li要素を追加
-      const list = document.createElement("li");
-      list.innerText = user.name;
-      lists.appendChild(list);
-    });
-  });
+  button.addEventListener("click", listUsers);
 
+  window.addEventListener("load", listUsers);
 }
