36fa58e42bb6b81931dddf83d4838555d07219ea
takefumi24 <t_ohba24@yahoo.co.jp>
2020-05-30 12:00:06 +0900

    カスタムデータ属性(dataset)

--- DOM/main.js
+++ DOM/main.js
@@ -3,7 +3,6 @@
 {
   document.querySelector('button').addEventListener('click', () => {
     const targetNode = document.getElementById('target')
-    //toggleメソッドを使い、クラス属性の追加や削除
-    targetNode.classList.toggle('my-color');
+    targetNode.textContent = targetNode.dataset.translation;
   });
 }
