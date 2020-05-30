f0d4ae0de3961fc1f129b525817a941032ce6998
takefumi24 <t_ohba24@yahoo.co.jp>
2020-05-30 11:50:20 +0900

    toggleメソッド

--- DOM/main.js
+++ DOM/main.js
@@ -3,11 +3,7 @@
 {
   document.querySelector('button').addEventListener('click', () => {
     const targetNode = document.getElementById('target')
-    //クラス属性の追加や削除
-    if (targetNode.classList.contains('my-color') === true) {
-      targetNode.classList.remove('my-color');
-    } else {
-      targetNode.classList.add('my-color');
-    }
+    //toggleメソッドを使い、クラス属性の追加や削除
+    targetNode.classList.toggle('my-color');
   });
 }
