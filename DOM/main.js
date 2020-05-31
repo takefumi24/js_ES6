02a914ab3ac5b49e03a677ada9ff8514ce944ed4
takefumi24 <t_ohba24@yahoo.co.jp>
2020-05-31 18:38:52 +0900

    値の取得とfocusメソッド

--- DOM/main.js
+++ DOM/main.js
@@ -2,10 +2,11 @@
 
 {
   document.querySelector('button').addEventListener('click', () => {
-    const item1 = document.querySelectorAll('li')[1];
-    //一部の古いブラウザでは使えない可能性がある
-    item1.remove();
-    //親Node.removeChild(削除するNode)
-    document.querySelector('ul').removeChild('item1');
+    const li = document.createElement('li');
+    const text = document.querySelector('input');
+    li.textContent = text.value;
+    document.querySelector('ul').appendChild(li);
+    text.value = ''
+    text.focus();
   });
 }
