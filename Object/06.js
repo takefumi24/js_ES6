e7f13c71bb70e2b3d8456fd1b2f36729c0aea8d9
takefumi24 <t_ohba24@yahoo.co.jp>
2020-06-05 13:11:50 +0900

    「オブジェクト06」言語化

---
+++ Object/06.js
@@ -0,0 +1,17 @@
+/*
+スプレッド構文に慣れよう
+*/
+
+'use strict';
+
+{
+  const otherScores = [10, 20];
+  const scores = [80, 90, 40, 70];
+  //scoresの末尾にotherScoresの要素を追加する
+
+  function sum(a, b) {
+    console.log(a + b);
+  }
+  //sum()という関数を使ってotherScoresの要素の合計を出す
+
+}
