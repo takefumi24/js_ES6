dfddd6558bd66baab9c925f46197b78ceb7c427a
takefumi24 <t_ohba24@yahoo.co.jp>
2020-06-05 11:12:11 +0900

    「オブジェクト編03」言語化

---
+++ Object/03.js
@@ -0,0 +1,18 @@
+/*
+配列とループ処理を組み合わせる
+*/
+
+'use strict';
+
+{
+  const scores = [80, 90, 40, 70];
+
+  console.log(`Score: ${scores[0]}`);
+  console.log(`Score: ${scores[1]}`);
+  console.log(`Score: ${scores[2]}`);
+
+  //上記のコンソール出力部分をfor文で書き換える
+
+  //配列の要素数の変化に強いfor文で書き換える
+
+}
