7eff7c2a0e289fe61be9bb241123271553847301
takefumi24 <t_ohba24@yahoo.co.jp>
2020-06-05 11:08:41 +0900

    不要フォルダ削除

--- DOM/main.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-{
-  document.querySelector('ul').addEventListener('click', (e) => {
-    if (e.target.nodeName === 'LI') {
-      e.target.classList.toggle('done');
-    }
-  });
-}
