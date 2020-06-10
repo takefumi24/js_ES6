7a5259b8ff246212783e9cce657145b557e4cb2a
takefumi24 <t_ohba24@yahoo.co.jp>
2020-06-10 20:30:19 +0900

    mapメソッド

---
+++ Udemy_ES6/map/main.js
@@ -0,0 +1,12 @@
+var cars = [
+  { type: '軽自動車', price: '安い' },
+  { type: '高級車', price: '高い' },
+];
+
+var prices = cars.map(function (car) {
+  return car.price;
+});
+
+console.log(prices);
+
+//mapは実務では、一覧表示する時によく使われる
