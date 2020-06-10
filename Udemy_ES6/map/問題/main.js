ac716f3d3be4ba383a0894f7e08b00f77a7b4000
takefumi24 <t_ohba24@yahoo.co.jp>
2020-06-10 20:40:01 +0900

    【問題】mapメソッド

---
+++ Udemy_ES6/map/問題/main.js
@@ -0,0 +1,10 @@
+var images = [
+  { height: "34px", width: "39px" },
+  { height: "54px", width: "19px" },
+  { height: "83px", width: "75px" },
+];
+
+var heights = images.map(function (image) {
+  return image.height;
+});
+console.log(heights);
