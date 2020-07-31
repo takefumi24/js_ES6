a5e556c5c82087b2c544379c5c26255f2af467e9
takefumi24 <t_ohba24@yahoo.co.jp>
2020-07-31 23:56:51 +0900

    「JavaScriptBasics編20」言語化

---
+++ JavaScriptBasics/20.js
@@ -0,0 +1,23 @@
+/*
+関数で処理をまとめてその関数を呼び出す
+
+ここでは関数でまとめてコードを書き換える練習をします
+*/
+
+//厳密なエラーチェック
+
+//以下のコードで重複しているところを関数にまとめてみましょう
+
+console.log('----------');
+console.log('--- Ad ---');
+console.log('----------');
+console.log('Tom is great!');
+console.log('Bob is great!');
+console.log('----------');
+console.log('--- Ad ---');
+console.log('----------');
+console.log('Steve is great!');
+console.log('Richard is great!');
+console.log('----------');
+console.log('--- Ad ---');
+console.log('----------');
